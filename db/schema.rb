72cd61918e092718c184aea92ca55f367d764a2b
Nicholas Powers <njpowers7915@gmail.com>
2018-06-28 12:38:44 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180628123601) do
+ActiveRecord::Schema.define(version: 20180628123822) do
 
   create_table "accounts", force: :cascade do |t|
     t.string   "name"
@@ -40,7 +40,7 @@ ActiveRecord::Schema.define(version: 20180628123601) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.integer  "category_id"
-    t.boolean  "post_status"
+    t.string   "post_status"
   end
 
 end
